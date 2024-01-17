5b38dc0c82c7c44dbbd5fb96052d976d514f7034
priyankkmt <149180584+priyankkmt@users.noreply.github.com>
2024-01-17 18:26:38 +0530

    Add files via upload

---
+++ AddPanel/AddPanel/CsAddPanel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Windows.Media.Imaging;
+
+namespace AddPanel
+{
+    public class CsAddPanel : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel("NewRibbonPanel");
+
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+            PushButtonData buttonData = new PushButtonData("cmdHelloWorld",
+               "Hello World", thisAssemblyPath, "AddPanel.HelloWorld");
+
+            PushButton pushButton = ribbonPanel.AddItem(buttonData) as PushButton;
+
+            pushButton.ToolTip = "Say hello to the people of cctech.";
+
+            Uri uriImage = new Uri(@"C:\Users\Priyank Kumawat\Downloads\peakpx.png");
+            BitmapImage largeImage = new BitmapImage(uriImage);
+            pushButton.LargeImage = largeImage;
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class HelloWorld : IExternalCommand
+    {
+        public Autodesk.Revit.UI.Result Execute(ExternalCommandData revit,
+            ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Revit", "Hello World");
+            return Autodesk.Revit.UI.Result.Succeeded;
+        }
+    }
+}
